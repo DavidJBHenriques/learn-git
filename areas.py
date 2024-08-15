e94cf4cb8dba74208e9e9f11be09c9f5dbf67ad8
DavidJBHenriques <141315337+DavidJBHenriques@users.noreply.github.com>
2024-08-15 19:13:48 +0100

    Create areas.py

---
+++ areas.py
@@ -0,0 +1,16 @@
+#TODO: Implement function that computes the area of a square of side s
+def square_area(s):
+    return 
+
+#TODO: Implement function that computes the area of a rectangle of sides w and l
+def rectangle_area(w,l):
+    return 
+
+#TODO: Implement function that returns the area of a cube of side s
+def cube_area(s):
+    return 
+
+
+#TODO: Implement function that returns the area of a circle with radius r
+def circle_area(r):
+    return 
